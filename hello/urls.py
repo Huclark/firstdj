193df3946df688d2608a35940cb773904917978c
Huclark <vhuclark@gmail.com>
2024-01-24 15:41:02 +0000

    Add: set index function as default path

--- hello/urls.py
+++ hello/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from . import views
 
 # create a list of all the allowable urls that can be
 # accessed for this particular app
+urlpatterns = [
+    path("", views.index, name="index")
+]
