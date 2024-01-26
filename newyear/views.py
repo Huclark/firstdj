8a0c27ee353001d1ff97904e16b8c4bc260417a3
Huclark <vhuclark@gmail.com>
2024-01-26 15:23:57 +0000

    Update doctsring

--- newyear/views.py
+++ newyear/views.py
@@ -3,6 +3,14 @@ from datetime import datetime
 
 # Create your views here.
 def index(request):
+    """_summary_
+
+    Args:
+        request (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return render(request, "newyear/index.html", {
         "newyear": "YES" if datetime.now().month == 1 and datetime.now().day == 1 else "NO"
     })
