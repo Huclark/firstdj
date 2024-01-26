d91bf201e0de9299d757c195dda9891b96f34ea9
Huclark <vhuclark@gmail.com>
2024-01-26 08:56:40 +0000

    Add: index method

--- newyear/views.py
+++ newyear/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
+def index(request):
+    return render(request, "newyear/index.html", {
+        "newyear": "YES" if datetime.now().month == 1 and datetime.now().day == 1 else "NO"
+    })
