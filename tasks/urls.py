4c25135550fd4a493b1d695c2458d72db731fd8b
Huclark <vhuclark@gmail.com>
2024-01-26 18:13:31 +0000

    Update: easy identification of routes

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+# add this so as to be more specific when calling routes
+app_name = "tasks"
+# create the paths
 urlpatterns = [
     path("", views.index, name="index"),
     path("add", views.add, name="add")
